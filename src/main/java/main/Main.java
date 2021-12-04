6b8961e5ea1a905f7ae7d4aefa588fe7b9eb1f57
Todiras Bogdan <bogdan.todiras@gmail.com>
2021-12-04 22:39:29 +0200

    4th commit

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,15 +1,8 @@
 package main;
 
-import model.Student;
-import repository.StudentJDBCRepository;
-
-import java.io.IOException;
-
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-
-
+    public static void main(String[] args) {
 
     }
 }
